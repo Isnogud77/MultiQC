1987787d458b07bd59b0ebddf665fe960b7b066e
Moritz Gilsdorf <moritz.gilsdorf@roche.com>
2021-06-02 08:55:01 +0200

    Fix calculation of aligned reads percentange

--- multiqc/modules/sentieon/AlignmentSummaryMetrics.py
+++ multiqc/modules/sentieon/AlignmentSummaryMetrics.py
@@ -99,10 +99,10 @@ def parse_reports(self):
             # Sentieon reports both reads for PE data.
             # Divide it by two as most people will expect # clusters
             if self.sentieon_alignment_metrics[s_name]["CATEGORY"] == "PAIR":
-                pdata[s_name]["total_reads"] = self.sentieon_alignment_metrics[s_name]["TOTAL_READS"] / 2
+                pdata[s_name]["total_reads"] = self.sentieon_alignment_metrics[s_name]["PF_READS"] / 2
                 pdata[s_name]["aligned_reads"] = self.sentieon_alignment_metrics[s_name]["PF_READS_ALIGNED"] / 2
             else:
-                pdata[s_name]["total_reads"] = self.sentieon_alignment_metrics[s_name]["TOTAL_READS"]
+                pdata[s_name]["total_reads"] = self.sentieon_alignment_metrics[s_name]["PF_READS"]
                 pdata[s_name]["aligned_reads"] = self.sentieon_alignment_metrics[s_name]["PF_READS_ALIGNED"]
                 pdata[s_name]["unaligned_reads"] = pdata[s_name]["total_reads"] - pdata[s_name]["aligned_reads"]
 
